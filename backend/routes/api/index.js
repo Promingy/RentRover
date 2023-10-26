efdd1be0509082284b39b4b0da2871e40defb095
Corbin Ainsworth <promingy@gmail.com>
2023-10-26 16:28:30 -0600

    Add User authentication Express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
+const { User } = require('../../db/models')
 
-router.post('/test', (req, res) => {
-    res.json({requestBody: req.body})
-})
+// Connect restoreUser middleware to the API router
+    // If current user session is valid, set req.user to the user in the database
+    // If current user session is not valid, set req.user to null
+
+router.use(restoreUser);
+
 
 module.exports = router;
-
