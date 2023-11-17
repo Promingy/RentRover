c8ec29ebd3e6edd45a373f40e91e28af16c67b5c
Corbin <140107765+Promingy@users.noreply.github.com>
2023-11-16 21:45:27 -0700

    finished sign up - phase 2, still need to make all errors show up at the same time opposed to one at a time

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,30 @@
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPageForm from './components/LoginPageForm';
+import SignupFormPage from './components/SignupFormPage/SignupFormPage';
+import * as sessionActions from './store/session';
+
 function App() {
-  return <h1> Hello from App </h1>;
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(sessionActions.thunkRestoreUser()).then(() => {
+      setIsLoaded(true)
+    })
+  }, [dispatch])
+
+  return (
+    <BrowserRouter>
+    { isLoaded &&
+      <Routes>
+        <Route path='/' element={<h1>Welcome!</h1>} />
+        <Route path='/login' element={<LoginPageForm />} />
+        <Route path='/signup' element={<SignupFormPage />} />
+      </Routes> }
+    </BrowserRouter>
+  )
 }
 
 export default App;
