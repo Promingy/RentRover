60677f2c21ddfa6d062e5ad36fdcc6ff200eb3b6
Corbin <140107765+Promingy@users.noreply.github.com>
2023-11-17 15:43:36 -0700

    fixed typo from copy pasting

--- frontend/src/components/SignupFormModal/SignupFormModal.jsx
+++ frontend/src/components/SignupFormModal/SignupFormModal.jsx
@@ -20,7 +20,7 @@ function SignupFormModal() {
     if (password === confirmPassword) {
       setErrors({});
       return dispatch(
-        sessionActions.signup({
+        sessionActions.thunkSignup({
           email,
           username,
           firstName,
