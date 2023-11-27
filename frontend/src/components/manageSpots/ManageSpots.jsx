90ac57ac87a5e14c49e553ba182af7b2d025e515
Corbin <140107765+Promingy@users.noreply.github.com>
2023-11-27 16:34:16 -0700

    added extra layer of security so that Create Spot, Update Spot, and Manage Spots aren't accessable by a user logged in. Even if they manually change the url

--- frontend/src/components/manageSpots/ManageSpots.jsx
+++ frontend/src/components/manageSpots/ManageSpots.jsx
@@ -9,46 +9,49 @@ import DeleteSpotModal from "../DeleteSpotModal";
 export default function ManageSpots() {
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    const sessionUser = useSelector((state) => state.session.user);
     const spots = useSelector(state => state.spots)
     const allSpots = spots.userSpots && Object.values(spots.userSpots)
 
     useEffect(() => {
+        if (!sessionUser) navigate('/');
         dispatch(thunkGetCurrentUserSpots())
-    }, [dispatch])
-
+    }, [dispatch, sessionUser, navigate])
 
     //most styling and formatting is in Spots.css
     return (
-        <div className="manageWrapper">
-            <header className="manageHeader">
-                <h1>Manage Your Spots</h1>
-                <button className='newSpotButton' onClick={() => navigate('/spots/new')}>Create a New Spot</button>
-            </header>
-            <ul className="spotsContainer">
-                {allSpots && Array.isArray(allSpots) && allSpots.map(spot => (
-                    <li title={`${spot.name}`} className='spots manageSpotsWrapper' key={`${spot.id}`} >
-                        <label className='manageSpots' onClick={() => navigate(`/spots/${spot.id}`)}>
-                            <img className='previewImage' src={`${spot.previewImage}`} alt={`${spot.previewImage}`} />
-                            <span className="locationRating">
-                                <p>{spot.city}, {spot.state}</p>
-                                <p className='starRating'>
-                                    <i className='fa-solid fa-star star' />
-                                    {typeof spot?.avgRating === 'number' && spot?.avgRating?.toFixed(1) || 'New'}
-                                </p>
-                            </span>
-                            <p className='locationPrice'>${spot.price} night</p>
+        <>
+            { sessionUser && <div className="manageWrapper">
+                <header className="manageHeader">
+                    <h1>Manage Your Spots</h1>
+                    <button className='newSpotButton' onClick={() => navigate('/spots/new')}>Create a New Spot</button>
+                </header>
+                <ul className="spotsContainer">
+                    {allSpots && Array.isArray(allSpots) && allSpots.map(spot => (
+                        <li title={`${spot.name}`} className='spots manageSpotsWrapper' key={`${spot.id}`} >
+                            <label className='manageSpots' onClick={() => navigate(`/spots/${spot.id}`)}>
+                                <img className='previewImage' src={`${spot.previewImage}`} alt={`${spot.previewImage}`} />
+                                <span className="locationRating">
+                                    <p>{spot.city}, {spot.state}</p>
+                                    <p className='starRating'>
+                                        <i className='fa-solid fa-star star' />
+                                        {typeof spot?.avgRating === 'number' && spot?.avgRating?.toFixed(1) || 'New'}
+                                    </p>
+                                </span>
+                                <p className='locationPrice'>${spot.price} night</p>
 
-                        </label>
-                        <div className="manageButtonsContainer">
-                            <button className="updateButton" onClick={() => navigate(`/spots/${spot.id}/edit`)}>Update</button>
-                            <label className='deleteButtonContainer'>
-                                <p className="deleteButtonText">Delete </p>
-                                <OpenModalButton modalComponent={<DeleteSpotModal spotId={spot.id}/>}/>
                             </label>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </div>
+                            <div className="manageButtonsContainer">
+                                <button className="updateButton" onClick={() => navigate(`/spots/${spot.id}/edit`)}>Update</button>
+                                <label className='deleteButtonContainer'>
+                                    <p className="deleteButtonText">Delete </p>
+                                    <OpenModalButton modalComponent={<DeleteSpotModal spotId={spot.id}/>}/>
+                                </label>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>}
+        </>
     )
 }
